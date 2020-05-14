a215d5b20fd4bd2a1687be7897ee91d1d65beea1
diaa <diaa.elsayedziada@gmail.com>
2020-05-14 23:32:29 +0200

    init commit

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd gender && cmake -H. -Bbuild && cd build && make && cd .. && cd ..
-cd expression && cmake -H. -Bbuild && cd build && make && cd .. && cd ..
-cd multiple && cmake -H. -Bbuild && cd build && make && cd .. && cd ..
-
--- download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.4.0%2Bcpu.zip
- unzip *.zip
-
